1eb5e4fa0847791477687181fc91844142649868
alicanselen <81762458+alicanselen@users.noreply.github.com>
2024-11-08 12:53:19 +0300

    validation with zod +drizzle schema

--- api/src/db/productsSchema.ts
+++ api/src/db/productsSchema.ts
@@ -1,4 +1,5 @@
 import { doublePrecision, integer, pgTable, text, varchar } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
 
 export const productsTable = pgTable("Products", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -7,3 +8,12 @@ export const productsTable = pgTable("Products", {
   image:varchar({ length: 255 }),
   price:doublePrecision().notNull(),
 });
+
+export const createProductSchema = createInsertSchema(productsTable).omit({
+  id:true, 
+});
+
+export const updateProductSchema = createInsertSchema(productsTable).omit({
+  id:true, 
+})
+.partial();
--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { listProducts ,getProductById ,updateProduct , createProduct , deleteProduct} from "./productsController";
+import { validateData } from "../..//middlewares/validatinMiddleware";
+import { createProductSchema , updateProductSchema} from "../../db/productsSchema";
 
 const router= Router();
 
 router.get('/' , listProducts);
 router.get('/:id' ,getProductById);
-router.post('/', createProduct);
-router.put('/:id' , updateProduct);
+router.post('/',validateData(createProductSchema), createProduct);
+router.put('/:id' ,validateData(updateProductSchema), updateProduct);
 router.delete('/:id' , deleteProduct);
 
 export default router;

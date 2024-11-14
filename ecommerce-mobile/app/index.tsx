3965fd014a0d4c3268e3a5aeeadc83d748178151
alicanselen <81762458+alicanselen@users.noreply.github.com>
2024-11-14 14:33:49 +0300

    GlueStack Kurulumlari Yapildi

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,12 +1,12 @@
 import { Text, View , FlatList } from "react-native";
 import products from  '../assets/products.json'
 import ProductListItem from "../components/productListItem";
+import { Button, ButtonText } from "@/components/ui/button";
 
 export default function HomeScreen(){
     return (
-        <FlatList
-            data={products}
-            renderItem={({item}) =><ProductListItem product = {item}/>}
-        /> 
+        <Button variant="outline">
+            <ButtonText>Basiniz Lutfen</ButtonText>
+        </Button>
     )
 }
